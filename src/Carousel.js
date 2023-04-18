68ced30235513bf733c135772230749ef52d77c8
Kevin Nguyen <kayvinhnguyen@gmail.com>
2023-04-18 16:30:29 -0700

    re

--- src/Carousel.js
+++ src/Carousel.js
@@ -25,24 +25,31 @@ import Card from "./Card";
     setCurrCardIdx(currCardIdx + 1);
   }
 
+  //Decrements currCardIdx state by 1
+  function goBackward() {
+    setCurrCardIdx(currCardIdx - 1);
+  }
+
   return (
     <div className="Carousel">
       <h1>{title}</h1>
       <div className="Carousel-main">
-        <i
+        {/* make hidden if on first, always have icon there */}
+        {currCardIdx !== 0 && <i
           className="bi bi-arrow-left-circle"
-          onClick={goForward}
-        />
+          onClick={goBackward}
+        />}
         <Card
           caption={currCard.caption}
           src={currCard.src}
           currNum={currCardIdx + 1}
           totalNum={total}
         />
-        <i
+        {/* make hidden if on last, always have icon there */}
+        {currCardIdx !== total - 1 && <i
           className="bi bi-arrow-right-circle"
           onClick={goForward}
-        />
+        />}
       </div>
     </div>
   );
--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -1,6 +1,7 @@
 import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 import TEST_IMAGES from "./_testCommon.js";
+import photos from "./photos";
 
 it("works when you click on the right arrow", function() {
   const { container } = render(
@@ -28,4 +29,76 @@ it("works when you click on the right arrow", function() {
   expect(
     container.querySelector('img[alt="testing image 2"]')
   ).toBeInTheDocument();
+});
+
+it("smoketest - renders without crashing", function () {
+  render(<Carousel 
+      photos={photos}
+      title="Shells from far-away beaches"
+  />);
+});
+// end
+
+it("matches snapshot", function () {
+  const { container } = render(
+    <Carousel 
+      photos={photos}
+      title="Shells from far-away beaches"
+    />);
+  expect(container).toMatchSnapshot();
+});
+
+it("works when you click on the left arrow", function () {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  // move forward in carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+
+  // move back in carousel
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  fireEvent.click(leftArrow);
+
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+
+});
+
+it("Hide arrow img on left if on first image", function () {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  expect(leftArrow).not.toBeInTheDocument();
+});
+
+it("Hide arrow img on right if on last image", function () {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+
+  // move forward to last carousel
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+
+  expect(rightArrow).not.toBeInTheDocument();
+  expect(container).toMatchSnapshot();
 });
